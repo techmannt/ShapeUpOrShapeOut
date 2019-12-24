05ff24a2e95fff326b13c69854dc24ab802bd76d
ntompkins <ntompkins@classworks.com>
2019-12-24 11:40:05 -0600

    Finalize according to instructions.

--- app.js
+++ app.js
@@ -1,38 +1,30 @@
-let shapesArr = [];
-let shapesCount = 1;
-
 class Shape {
   constructor(width, height, shapeName) {
-    this.width = width;
-    this.height = height;
+    this.width = Number(width);
+    this.height = Number(height);
     this.shapeName = shapeName;
 
     this.myDiv = document.createElement('div');
     this.myDiv.classList.add('shape');
     this.myDiv.classList.add(this.shapeName);
-    this.myDiv.classList.add(shapesCount);  // Add unique # as a class to each shape.
 
     this.computeRandomLocation();  // This will happen EVERY time 1 of the shapes is instantiated with "new" since we're using "extends" on the class declaration.
-    // '<div class="shape"></div>';
     this.setupStyles();
 
-    $(`.${shapesCount}`).click(() => {
-      // console.log(shapesArr[shapesCount-1]);
-      // console.log(`you clicked on: ${shapesArr[shapesCount-1].shape}`);
-      this.describe();
-
-      // $('#shapeHeightField').text(this.)
+    this.myDiv.addEventListener('click', () => this.describe());
+    this.myDiv.addEventListener('dblclick', () => {
+      document.getElementById('canvas').removeChild(this.myDiv);
     })
-    shapesCount++;
+
   };
 
   describe() {
     $('#shapeNameField').text(this.shapeName);
-    $('#shapeHeightField').text(shapesArr[shapesCount - 1].height);
-    $('#shapeWidthField').text(shapesArr[shapesCount - 1].width);
-    $('#shapeAreaField').text(shapesArr[shapesCount - 1].area);
-    $('#shapePerimeterField').text(shapesArr[shapesCount - 1].perimeter);
-    $('#shapeRadiusField').text(shapesArr[shapesCount - 1].radius);
+    $('#shapeHeightField').text(this.height);
+    $('#shapeWidthField').text(this.width);
+    $('#shapeAreaField').text(this.height * this.width);
+    $('#shapePerimeterField').text(2*this.height + 2*this.width);
+    $('#shapeRadiusField').text('N/A');
   }
 
   computeRandomLocation() {
@@ -53,9 +45,7 @@ class Shape {
     this.myDiv.style.top = `${this.randomY}px`;
     this.myDiv.style.width = `${this.width}px`;
     this.myDiv.style.height = `${this.height}px`;
-    console.log(this.myDiv);  // For debugging...
     document.getElementById('canvas').appendChild(this.myDiv);
-    // $('canvas').append(myDiv);
   }
 
 }
@@ -63,15 +53,19 @@ class Shape {
 class Circle extends Shape {
   constructor(radius) {
     super(`${radius * 2}`, `${radius * 2}`, 'circle');
-    this.radius = radius;
-    this.addToArray();
+    this.radius = Number(radius);
   }
 
   area = () => Math.PI * (this.radius * this.radius);
   perimeter = () => 2 * Math.PI * this.radius;
 
-  addToArray() {
-    shapesArr.push({ shape: "circle", height: "N/A", width: "N/A", area: this.area(), perimeter: this.perimeter(), radius: this.radius });
+  describe() {
+    $('#shapeNameField').text(this.shapeName);
+    $('#shapeHeightField').text(this.height);
+    $('#shapeWidthField').text(this.width);
+    $('#shapeAreaField').text(this.area());
+    $('#shapePerimeterField').text(this.perimeter());
+    $('#shapeRadiusField').text(this.radius);
   }
 
 }
@@ -79,12 +73,9 @@ class Circle extends Shape {
 class Triangle extends Shape {
   constructor(height) {
     super(height, height, 'triangle');
-    this.height = height;
-    this.addToArray();
   }
 
   setupStyles() {
-    console.log('Function is overridden!');  // For debugging...
     this.myDiv.style.left = `${this.randomX}px`;
     this.myDiv.style.top = `${this.randomY}px`;
     this.myDiv.style.width = `0px`;
@@ -96,45 +87,53 @@ class Triangle extends Shape {
   }
 
   area = () => 0.5 * this.width * this.height;
-  perimeter = () => (this.width * 2) + (this.height * 2);
+  perimeter = () => this.hypotenuse() + this.width + this.height;
+  hypotenuse = () => Math.sqrt(this.height * this.height + this.width * this.width);
 
-  addToArray() {
-    shapesArr.push({ shape: "triangle", height: this.height, width: this.height, area: this.area(), perimeter: this.perimeter(), radius: "N/A" });
+  describe() {
+    $('#shapeNameField').text('triangle');
+    $('#shapeHeightField').text(this.height);
+    $('#shapeWidthField').text(this.width);
+    $('#shapeAreaField').text(this.area());
+    $('#shapePerimeterField').text(this.perimeter());
+    $('#shapeRadiusField').text('N/A');
   }
-
 }
 
 class Rectangle extends Shape {
   constructor(width, height) {
-    super(width, height, 'rectangle');
-    this.width = width;
-    this.height = height;
-    this.testFunc();
-    this.addToArray();
+    super(width, height, 'rectangle');  // No "this.width" or "this.height" needed here!
   }
 
   area = () => this.width * this.height;
   perimeter = () => (this.width * 2) + (this.height * 2);
-  testFunc = () => console.log(`the area of the rectangle is: ${this.area()}`);
 
-  addToArray() {
-    shapesArr.push({ shape: "rectangle", height: this.height, width: this.width, area: this.area(), perimeter: this.perimeter(), radius: "N/A" });
+  describe() {
+    $('#shapeNameField').text('rectangle');
+    $('#shapeHeightField').text(this.height);
+    $('#shapeWidthField').text(this.width);
+    $('#shapeAreaField').text(this.area());
+    $('#shapePerimeterField').text(this.perimeter());
+    $('#shapeRadiusField').text('N/A');
   }
 
 }
 
 class Square extends Shape {
-  constructor(length) {  // with a square, ALL 4 sides are equal so I only need 1 value: this.width.
+  constructor(length) {  // with a square, ALL 4 sides are equal so I only need 1 value: length.
     super(length, length, 'square');
-    this.length = length;
-    this.addToArray();
   }
 
   area = () => this.width * this.width;
   perimeter = () => this.width * 4;
 
-  addToArray() {
-    shapesArr.push({ shape: "square", height: this.length, width: this.length, area: this.area(), perimeter: this.perimeter(), radius: "N/A" });
+  describe() {
+    $('#shapeNameField').text('square');
+    $('#shapeHeightField').text(this.height);
+    $('#shapeWidthField').text(this.width);
+    $('#shapeAreaField').text(this.area());
+    $('#shapePerimeterField').text(this.perimeter());
+    $('#shapeRadiusField').text('N/A');
   }
 
 }
@@ -154,7 +153,6 @@ $('#addSquare').click(() => {
   let squareLength = $('#squareLength').val();
   let square = new Square(squareLength, squareLength);
 })
-
 
 $('#addCircle').click(() => {
   let radius = $('#circleRadius').val();
